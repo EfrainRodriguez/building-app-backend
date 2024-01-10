4f2a1accecb9d917f65025b199723a6d22fc8f3f
EfrainRodriguez <efrainrg92@gmail.com>
2024-01-10 05:05:23 -0500

    adding swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
+import { RequestMethod } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
+
+const API_PREFIX = 'api';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger
+  const config = new DocumentBuilder()
+    .setTitle('BuildingApp API')
+    .setDescription('This is the API documentation for BuildingApp')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  // CORS
+  app.enableCors();
+
+  // Prefix
+  app.setGlobalPrefix(API_PREFIX, {
+    exclude: [{ path: '/', method: RequestMethod.GET }]
+  });
+
   await app.listen(3000);
 }
 bootstrap();
---
+++ src/modules/project/dtos/create-project.dto.ts
@@ -0,0 +1,58 @@
+import {
+  IsString,
+  MaxLength,
+  MinLength,
+  IsDateString,
+  IsOptional,
+  IsArray
+} from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateProjectDto {
+  @ApiProperty({
+    description: 'The name of the project',
+    minLength: 3,
+    maxLength: 50,
+    example: 'My Project'
+  })
+  @IsString()
+  @MinLength(3)
+  @MaxLength(50)
+  name: string;
+
+  @ApiProperty({
+    description: 'The start date of the project',
+    example: '2020-01-01'
+  })
+  @IsString()
+  @IsDateString()
+  startDate: string;
+
+  @ApiProperty({
+    description: 'The end date of the project',
+    example: '2020-12-31'
+  })
+  @IsString()
+  @IsDateString()
+  endDate: string;
+
+  @ApiProperty({
+    description: 'Images of the project',
+    example: ['https://example.com/image1.jpg']
+  })
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
+  images: string[];
+
+  @ApiProperty({
+    description: 'Items of the project',
+    example: [{ name: 'Item 1', unitPrice: 100 }]
+  })
+  @IsOptional()
+  @IsArray()
+  items: {
+    name: string;
+    unitPrice: number;
+  }[];
+}
